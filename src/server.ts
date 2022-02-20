331789afecf0856508b737eda9fc6de4215c91ca
George-b1t <george.soares509@gmail.com>
2022-02-19 22:42:57 -0300

    first-commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.json({
+    message: "Hello World",
+  });
+});
+
+app.listen(3333, () => {
+  console.log("🔥 Server is running");
+});
